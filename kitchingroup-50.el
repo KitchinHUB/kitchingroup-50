7c2b02b6ff1f3c613134d812c5a9f3333f66b636
John Kitchin <jkitchin@andrew.cmu.edu>
2016-02-01 19:21:34 -0500

    init

---
+++ kitchingroup-50.el
@@ -0,0 +1,38 @@
+;;; kitchingroup-50.el --- CaPPA preprint for doi:10.1021/jp511426q
+
+;; Copyright (C) 2016 John Kitchin
+
+;; Version: 0.0.1
+;; Author: Zhongnan Xu
+;;   Jan Rossmeisl
+;;   John Kitchin <jkitchin@andrew.cmu.edu>
+;; Keywords:
+;; DOI: 10.1021/jp511426q
+;; Journal: The Journal of Physical Chemistry C
+;; Bibtex: @article{xu-2015-linear-respon,
+;;   author =	 {Xu, Zhongnan and Rossmeisl, Jan and Kitchin, John R.},
+;;   title =	 {A Linear Response {DFT}+{U} Study of Trends in the Oxygen
+;;                   Evolution Activity of Transition Metal Rutile Dioxides},
+;;   journal =	 {The Journal of Physical Chemistry C},
+;;   volume =	 119,
+;;   number =	 9,
+;;   pages =	 {4827-4833},
+;;   year =	 2015,
+;;   doi =	 {10.1021/jp511426q},
+;;   url =	 { http://dx.doi.org/10.1021/jp511426q },
+;;   eprint =	 { http://dx.doi.org/10.1021/jp511426q },
+;;  }
+
+
+;;; Commentary:
+
+;; Put some descriptive text here.
+
+;;; Code:
+(require 'cappa)
+
+(cappa-register 'kitchingroup-50)
+
+
+(provide 'kitchingroup-50)
+;;; kitchingroup-50.el ends here
